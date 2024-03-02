c31cbe5c7287ec418fdf4552ef48be03a82136b4
ruchithaEtikayala98 <etikayala.ruchitha-reddy@e.lloydsbanking.com>
2024-03-02 16:31:14 +0000

    Implemented CORS

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const cors = require('cors')
 
 const indexRouter = require('./routes/moviesRouter');
 
@@ -11,6 +12,11 @@ const app = express();
 // view engine setup
 // app.set('views', path.join(__dirname, 'views'));
 // app.set('view engine', 'jade');
+
+//Implement CORS
+app.use(cors())
+
+app.options('*',cors())
 
 app.use(logger('dev'));
 app.use(express.json());
